f51458f235e8b3ad62e101fd2e8a244eae7c9f61
Sam-GTECH <sburton@gaming.tech>
2022-12-05 18:38:35 +0100

    Linked to pokedex database

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb-legacy");
 const connectionString =
-  "mongodb+srv://pokedexApp:tryed7K5R8wQ4Wb0@cluster0.ldrf8ni.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://app:oEHwL9yaYzt0Aopg@Projet5Pokedex.shjpvyn.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -14,8 +14,7 @@ module.exports = {
       if (err || !db) {
         return err;
       }
-      
-      //remplacer whatever par le nom de votre DB !
+
       dbConnection = db.db("pokedex");
       console.log("Successfully connected to MongoDB.");
     });
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const dbo = require("./db/db");
 const app = express();
-const port = 4443;
+const port = 4444;
 
 dbo.connectToServer();
-
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
@@ -13,3 +12,27 @@ app.get("/", function (req, res) {
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
+
+/* index.js code before... */
+app.get("/pokemon/list", function (req, res) {
+    //on se connecte à la DB MongoDB
+    const dbConnect = dbo.getDb();
+    //premier test permettant de récupérer mes pokemons !
+    dbConnect
+      .collection("Pokemon")
+      .find({}) // permet de filtrer les résultats
+      /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching pokemons!");
+        } else {
+          res.json(result);
+        }
+      });
+      /*
+      Bref lisez la doc, 
+      il y a plein de manières de faire ce qu'on veut :) 
+      */
+      
+  });
+  
