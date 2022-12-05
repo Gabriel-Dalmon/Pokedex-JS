fca924096ca4171fcb8ec31d02f1779bc8d25c1d
Sam-GTECH <sburton@gaming.tech>
2022-12-05 19:59:16 +0100

    unfinished index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dbo = require("./db/db");
+const bodyParser = require('body-parser');
 const app = express();
-const port = 4444;
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 4443;
+const Pokemon = require('pokemon.js');
+Pokemon.setLanguage('japanese');
 
 dbo.connectToServer();
+
+const jsonParser = bodyParser.json();
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
@@ -15,24 +21,60 @@ app.listen(port, function () {
 
 /* index.js code before... */
 app.get("/pokemon/list", function (req, res) {
-    //on se connecte à la DB MongoDB
-    const dbConnect = dbo.getDb();
-    //premier test permettant de récupérer mes pokemons !
-    dbConnect
-      .collection("Pokemon")
-      .find({}) // permet de filtrer les résultats
-      /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
-      .toArray(function (err, result) {
-        if (err) {
-          res.status(400).send("Error fetching pokemons!");
-        } else {
-          res.json(result);
-        }
-      });
-      /*
-      Bref lisez la doc, 
-      il y a plein de manières de faire ce qu'on veut :) 
-      */
-      
-  });
+  //on se connecte à la DB MongoDB
+  const dbConnect = dbo.getDb();
+  //premier test permettant de récupérer mes pokemons !
+  dbConnect
+    .collection("Pokemon")
+    .find({}) // permet de filtrer les résultats
+    /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching pokemons!");
+      } else {
+        res.json(result);
+      }
+    });
+    /*
+    Bref lisez la doc, 
+    il y a plein de manières de faire ce qu'on veut :) 
+    */
+    
+});
+
+app.post('/pokemon/insert', jsonParser, (req, res) => {
+	const body = req.body;
+	const dbConnect = dbo.getDb();
+
+	console.log('Got body:', body);
+
+	dbConnect
+		.collection("Pokemon")
+		.insertOne(body);
+	res.json(body);
+});
+
+app.post('/pokemon/remove', jsonParser, (req, res) => {
+	const body = req.body;
+	const dbConnect = dbo.getDb();
   
+	console.log('Got body:', body);
+  
+	dbConnect
+	  .collection("Pokemon")
+	  .deleteOne(body);
+	res.json(body);
+  });
+
+app.post('/pokemon/update', jsonParser, (req, res) => {
+	const body = req.body;
+	const dbConnect = dbo.getDb();
+  
+	console.log('Got body:', body);
+	console.log(body["get"])
+  
+	/*dbConnect
+	  .collection("Pokemon")
+	  .updateOne(body);*/
+	res.json(body);
+  });
