f536dddb6cd584c219eaf1e4af72714d53d7a2f7
Gabriel D <dalmgabi@gmail.com>
2022-12-05 18:37:51 +0100

    Added pokemons list request

--- db/db.js
+++ db/db.js
@@ -1,27 +1,26 @@
 const { MongoClient } = require("mongodb");
-const connectionString =
-  "mongodb+srv://pokedexApp:tryed7K5R8wQ4Wb0@cluster0.ldrf8ni.mongodb.net/?retryWrites=true&w=majority";
+const connectionString = 
+              "mongodb+srv://pokedexApp:ZHmInrL4Cxo1Vllq@cluster0.ldrf8ni.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 let dbConnection;
 
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (err || !db) {
-        return err;
-      }
-      
-      //remplacer whatever par le nom de votre DB !
-      dbConnection = db.db("pokedex");
-      console.log("Successfully connected to MongoDB.");
-    });
-  },
+    connectToServer: function (callback) {
+      client.connect(function (err, db) {
+        if (err || !db) {
+          return err;
+        }
 
-  getDb: function () {
-    return dbConnection;
-  },
+        dbConnection = db.db("pokedex");
+        console.log("Successfully connected to MongoDB.");
+      });
+    },
+
+    getDb: function () {
+        return dbConnection;
+    },
 };
