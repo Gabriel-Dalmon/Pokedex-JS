50bdffc69e8523ea295f0afb7a71b7ead593dde0
Sam-GTECH <sburton@gaming.tech>
2022-12-05 18:45:47 +0100

    Fucking mergemmmmmmmmmm

--- index.js
+++ index.js
@@ -9,32 +9,10 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.get("/pokedex/list", function (req, res) {
-    //on se connecte à la DB MongoDB
-    const dbConnect = dbo.getDb();
-    //premier test permettant de récupérer mes pokemons !
-    dbConnect
-      .collection("pokemons")
-      .find({}) // permet de filtrer les résultats
-      /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
-      .toArray(function (err, result) {
-        if (err) {
-          res.status(400).send("Error fetching pokemons!");
-        } else {
-          res.json(result);
-        }
-      });
-      /*
-      Bref lisez la doc, 
-      il y a plein de manières de faire ce qu'on veut :) 
-      */
-      
-  });
-
-
 app.listen(port, function () {
   console.log(`App listening on port ${port}!`);
 });
+
 /* index.js code before... */
 app.get("/pokemon/list", function (req, res) {
     //on se connecte à la DB MongoDB
