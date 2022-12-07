f527c9a8a21c04c9c22f1c5941d84f256677ea3d
Gabriel D <dalmgabi@gmail.com>
2022-12-07 10:00:07 +0100

    Router Gabriel

--- client/pokemon-js-react/src/App.js
+++ client/pokemon-js-react/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import './pages/AdminBoard';
+import './pages/HomePage';
+import './pages/PokedexPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/"> //ici on met l'URL dans le navigateur
+            <HomePage /> //ici on donne la page à afficher en fonction de cette URL
+          </Route>
+          <Route path="/pokedex">
+            <PokedexPage />
+          </Route>
+          <Route path="/adminpanel">
+            <AdminBoard />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
