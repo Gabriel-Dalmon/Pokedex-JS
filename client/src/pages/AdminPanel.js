98d9361b3000a316f98965e41aea41bec06944e9
Gabriel D <dalmgabi@gmail.com>
2022-12-08 11:28:13 +0100

    Added PokeCard Comp

--- client/src/pages/AdminPanel.js
+++ client/src/pages/AdminPanel.js
@@ -1,9 +1,11 @@
+import PokeCard from '../components/PokeCard';
 import Menu from './../components/Menu';
 
 function AdminPanel(props) {
     return <>
         <Menu />
         <h1>Admin Board</h1>
+        <PokeCard />
     </>
 }
 
