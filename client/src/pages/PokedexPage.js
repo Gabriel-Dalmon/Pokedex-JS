78fed622adf6368c727273c33e40e151f409a78c
Sam-GTECH <sburton@gaming.tech>
2022-12-08 12:02:36 +0100

    Add PokeBox

--- client/src/pages/PokedexPage.js
+++ client/src/pages/PokedexPage.js
@@ -1,9 +1,12 @@
+import PokeBox from '../components/PokeBox';
 import Menu from './../components/Menu';
 
 function PokedexPage(props) {
     return <>
         <Menu />
         <h1>Pokedex</h1>
+
+        <PokeBox name="Reshiram" img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/643.png" type1="fire" type2="dragon" />
     </>
 }
 
